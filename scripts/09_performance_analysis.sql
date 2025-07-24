289ddfbaac854cae53543b5b078a89da975a4f10
kbeduhn <kasey.beduhn@gmail.com>
2025-07-24 14:09:58 -0500

    Update and rename performance_analysis to 09_performance_analysis.sql

--- scripts/09_performance_analysis.sql
+++ scripts/09_performance_analysis.sql
@@ -2,9 +2,9 @@
 
 WITH yearly_product_sales AS (
 SELECT
-YEAR(f.order_date) AS order_year,
-p.product_name,
-SUM(f.sales_amount) AS current_sales
+	YEAR(f.order_date) AS order_year,
+	p.product_name,
+	SUM(f.sales_amount) AS current_sales
 FROM gold.fact_sales f
 LEFT JOIN gold.dim_products p
 ON f.product_key = p.product_key
@@ -14,15 +14,16 @@ YEAR(f.order_date),
 p.product_name
 )
 SELECT
-order_year,
-product_name,
-current_sales,
-AVG(current_sales) OVER (PARTITION BY product_name) avg_sales,
-current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
-CASE WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
-	WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
-	ELSE 'Avg'
-END avg_change,
+	order_year,
+	product_name,
+	current_sales,
+	AVG(current_sales) OVER (PARTITION BY product_name) avg_sales,
+	current_sales - AVG(current_sales) OVER (PARTITION BY product_name) AS diff_avg,
+	CASE WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) > 0 THEN 'Above Avg'
+		WHEN current_sales - AVG(current_sales) OVER (PARTITION BY product_name) < 0 THEN 'Below Avg'
+		ELSE 'Avg'
+	END avg_change,
+	
 --Year-Over-Year Analysis
 LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS py_sales,
 current_sales - LAG(current_sales) OVER (PARTITION BY product_name ORDER BY order_year) AS diff_py,
